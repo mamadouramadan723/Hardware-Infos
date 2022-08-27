44704f28698953e836f630b88603a60bece6efe3
RMD <72153331+mamadouramadan723@users.noreply.github.com>
2022-08-27 21:24:10 +0100

    5. get Cellular Connection Infos

--- app/src/main/java/com/rmd/media/ml/tf/myapplication/MainActivity.kt
+++ app/src/main/java/com/rmd/media/ml/tf/myapplication/MainActivity.kt
@@ -17,12 +17,18 @@ import android.text.format.Formatter
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.rmd.media.ml.tf.myapplication.databinding.ActivityMainBinding
+import java.net.InetAddress
+import java.net.NetworkInterface
+import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+
+    private var publicIP: String = ""
     private var systemInfos: String = ""
+    private var connectionInfos: String = ""
 
 
     @SuppressLint("HardwareIds")
@@ -32,10 +38,24 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.checkConnectionBtn.setOnClickListener {
+            getMyPublicIPAddress()
             checkConnectionAndShowInfosIfConnected(this)
         }
         binding.showSystemInfosBtn.setOnClickListener {
             getHardwareInfos()
+        }
+    }
+
+    private fun getMyPublicIPAddress() {
+        val interfaces: List<NetworkInterface> =
+            Collections.list(NetworkInterface.getNetworkInterfaces())
+        for (networkInterface in interfaces) {
+            val addressList: List<InetAddress> = Collections.list(networkInterface.inetAddresses)
+            for (address in addressList) {
+                if (!address.isLoopbackAddress) {
+                    publicIP = address.hostAddress?.toString() ?: ""
+                }
+            }
         }
     }
 
@@ -55,26 +75,26 @@ class MainActivity : AppCompatActivity() {
         val batteryLevel = batteryManager.getIntProperty(BatteryManager.BATTERY_PROPERTY_CAPACITY)
 
         if (SDK_INT >= Build.VERSION_CODES.M) {
-            systemInfos = "Brand: ${Build.BRAND} \n" +
-                    "DeviceID: ${
+            systemInfos = "\tBrand: ${Build.BRAND} \n" +
+                    "\tDeviceID: ${
                         Settings.Secure.getString(contentResolver, Settings.Secure.ANDROID_ID)
                     } \n" +
-                    "Model: ${Build.MODEL} \n" +
-                    "ID: ${Build.ID} \n" +
-                    "SDK: $SDK_INT \n" +
-                    "Manufacture: ${Build.MANUFACTURER} \n" +
-                    "Brand: ${Build.BRAND} \n" +
-                    "User: ${Build.USER} \n" +
-                    "Type: ${Build.TYPE} \n" +
-                    "Base: ${Build.VERSION_CODES.BASE} \n" +
-                    "Incremental: ${Build.VERSION.INCREMENTAL} \n" +
-                    "Board: ${Build.BOARD} \n" +
-                    "Host: ${Build.HOST} \n" +
-                    "FingerPrint: ${Build.FINGERPRINT} \n" +
-                    "Version Code: ${Build.VERSION.RELEASE} \n" +
-                    "Total Memory : $totalMemory GB\n" +
-                    "Available Memory : $availMemory GB\n" +
-                    "Battery Level : $batteryLevel %"
+                    "\tModel: ${Build.MODEL} \n" +
+                    "\tID: ${Build.ID} \n" +
+                    "\tSDK: $SDK_INT \n" +
+                    "\tManufacture: ${Build.MANUFACTURER} \n" +
+                    "\tBrand: ${Build.BRAND} \n" +
+                    "\tUser: ${Build.USER} \n" +
+                    "\tType: ${Build.TYPE} \n" +
+                    "\tBase: ${Build.VERSION_CODES.BASE} \n" +
+                    "\tIncremental: ${Build.VERSION.INCREMENTAL} \n" +
+                    "\tBoard: ${Build.BOARD} \n" +
+                    "\tHost: ${Build.HOST} \n" +
+                    //"\tFingerPrint: ${Build.FINGERPRINT} \n" +
+                    "\tVersion Code: ${Build.VERSION.RELEASE} \n" +
+                    "\tTotal Memory : $totalMemory GB\n" +
+                    "\tAvailable Memory : $availMemory GB\n" +
+                    "\tBattery Level : $batteryLevel %"
 
         } else {
             TODO("VERSION.SDK_INT < M")
@@ -112,16 +132,17 @@ class MainActivity : AppCompatActivity() {
                     val ssid = wInfo.ssid
                     val hssid = wInfo.hiddenSSID
 
-                    val infos = "Type :\tWIFI\n" +
+                    connectionInfos = "Type :\tWIFI\n" +
                             "IP Address:\t$ipAddress\n" +
                             "Link Speed:\t$linkSpeed\n" +
                             "Network ID:\t$networkID\n" +
                             "SSID:\t$ssid\n" +
                             "Hidden SSID:\t$hssid\n"
 
-                    binding.showConnectionInfosTv.text = infos
+                    binding.showConnectionInfosTv.text = connectionInfos
 
                 } else if (activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR)) {
+
 
                     val type = when (connectivityManager.activeNetworkInfo?.subtype) {
                         TelephonyManager.NETWORK_TYPE_GPRS, TelephonyManager.NETWORK_TYPE_EDGE, TelephonyManager.NETWORK_TYPE_CDMA, TelephonyManager.NETWORK_TYPE_1xRTT, TelephonyManager.NETWORK_TYPE_IDEN, TelephonyManager.NETWORK_TYPE_GSM -> "2G"
@@ -130,7 +151,8 @@ class MainActivity : AppCompatActivity() {
                         TelephonyManager.NETWORK_TYPE_NR -> "5G"
                         else -> "?"
                     }
-                    val infos = "\tCellular : $type\n"
+                    val infos = "\tCellular : $type\n" +
+                            "\tPublic IP : $publicIP\n"
                     binding.showConnectionInfosTv.text = infos
 
                 }
@@ -146,7 +168,6 @@ class MainActivity : AppCompatActivity() {
 
     companion object {
         private const val REQUEST_CODE = 100
-        private const val RC_PHONE_NUMBER = 101
     }
 
 
